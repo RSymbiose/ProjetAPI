882a9b7e42b417f7f3c8ce773636bb5f21ec82e4
RSymbiose <118276398+RSymbiose@users.noreply.github.com>
2025-01-31 17:46:43 +0100

    Update firebase.js

--- projetapi/src/firebase.js
+++ projetapi/src/firebase.js
@@ -4,15 +4,7 @@ import { getFirestore } from "firebase/firestore"; // Pour Firestore
 import { getAuth } from "firebase/auth"; // Pour l'authentification
 
 // La configuration Firebase de ton projet
-const firebaseConfig = {
-  apiKey: "AIzaSyA2Dp6AsNQrWMM7Bw5YXQA0kSgu_hRpycI",
-  authDomain: "projetapi-fa3f3.firebaseapp.com",
-  projectId: "projetapi-fa3f3",
-  storageBucket: "projetapi-fa3f3.firebasestorage.app",
-  messagingSenderId: "345360416678",
-  appId: "1:345360416678:web:61992bd927c18ba39bd967",
-  measurementId: "G-F4YZYTN1HK"
-};
+
 
 // Initialiser Firebase avec la configuration
 const app = initializeApp(firebaseConfig);
